008c921069ad94aad978a834a04d5efec70f9ad6
Valter-Kaspar Karolin <valzebnik@hot.ee>
2015-06-28 21:48:50 +0300

    Finished with basic layout

---
+++ app/build/build.bat
@@ -0,0 +1,4 @@
+CALL r.js.cmd -o app.build.js
+@cd ../../dist
+@rd /s /q build
+@PAUSE > nul
---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+require.config({
+	paths: {
+		"jquery": "vendor/jquery/dist/jquery",
+		"underscore": "vendor/underscore/underscore",
+		"backbone": "vendor/backbone/backbone",
+		"bootstrap": "vendor/bootstrap/dist/js/bootstrap"
+	}	
+});
+
+require(['views/app'], function(AppView) {
+	new AppView;
+});
